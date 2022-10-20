603c058309e7b5c22546778ce6d95609cce2657c
quangdlm <minhquang26116@gmail.com>
2022-10-20 23:15:53 +0700

    update

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    // baseURL: 'https://gig-worker-backend.azurewebsites.net',
-    baseURL: 'http://localhost:8080/',
+    baseURL: 'https://gig-worker-backend.azurewebsites.net',
+    // baseURL: 'http://localhost:8080/',
     headers: {
         'Contend-Type': 'application/json',
     }
