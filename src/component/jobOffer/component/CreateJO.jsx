09b3f1b7bc1954fbfe6d41a245e138dc388572a4
kongou <giachan9@gmail.com>
2022-11-08 20:02:18 +0700

    update create

--- src/component/jobOffer/component/CreateJO.jsx
+++ src/component/jobOffer/component/CreateJO.jsx
@@ -4,10 +4,13 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import jobOfferApi from '../../../api/JobOffer';
 import "./style.scss";
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
 
 const provivince = ["Thành phố Cần Thơ", "Thành phố Đà Nẵng", "Thành phố Hà Nội", "Thành phố Hải Phòng", "Thành phố Hồ Chí Minh", "Tỉnh An Giang", "Tỉnh Bà Rịa - Vũng Tàu", "Tỉnh Bắc Giang", "Tỉnh Bắc Kạn", "Tỉnh Bạc Liêu", "Tỉnh Bắc Ninh", "Tỉnh Bến Tre", "Tỉnh Bình Định", "Tỉnh Bình Dương", "Tỉnh Bình Phước", "Tỉnh Bình Thuận", "Tỉnh Cà Mau", "Tỉnh Cao Bằng", "Tỉnh Đắk Lắk", "Tỉnh Đắk Nông", "Tỉnh Điện Biên", "Tỉnh Đồng Nai", "Tỉnh Đồng Tháp", "Tỉnh Gia Lai", "Tỉnh Hà Giang", "Tỉnh Hà Nam", "Tỉnh Hà Tĩnh", "Tỉnh Hải Dương", "Tỉnh Hậu Giang", "Tỉnh Hoà Bình", "Tỉnh Hưng Yên", "Tỉnh Khánh Hòa", "Tỉnh Kiên Giang", "Tỉnh Kon Tum", "Tỉnh Lai Châu", "Tỉnh Lâm Đồng", "Tỉnh Lạng Sơn", "Tỉnh Lào Cai", "Tỉnh Long An", "Tỉnh Nam Định", "Tỉnh Nghệ An", "Tỉnh Ninh Bình", "Tỉnh Ninh Thuận", "Tỉnh Phú Thọ", "Tỉnh Phú Yên", "Tỉnh Quảng Bình", "Tỉnh Quảng Nam", "Tỉnh Quảng Ngãi", "Tỉnh Quảng Ninh", "Tỉnh Quảng Trị", "Tỉnh Sóc Trăng", "Tỉnh Sơn La", "Tỉnh Tây Ninh", "Tỉnh Thái Bình", "Tỉnh Thái Nguyên", "Tỉnh Thanh Hóa", "Tỉnh Thừa Thiên Huế", "Tỉnh Tiền Giang", "Tỉnh Trà Vinh", "Tỉnh Tuyên Quang", "Tỉnh Vĩnh Long", "Tỉnh Vĩnh Phúc", "Tỉnh Yên Bái"]
 
 function CreateJO() {
+    // validate form with formik and yup with materialUI numOfRecruit offerEndTime salary age startTime endTime jobDescription other business location province address degreejobType
     const user = JSON.parse(localStorage.getItem("FWApp-gig:rememberedAccount"));
     const [select, setSelect] = useState()
     const [repo, setRepo] = useState()
@@ -16,18 +19,77 @@ function CreateJO() {
     const [data, setData] = useState({
         accountID: user?.id,
     })
-    useEffect(() => {
-        setLoading(true)
-        axios.get(`https://gig-worker-backend.azurewebsites.net/Location/City?province=${select}`)
-            .then((res) => {
-                const { data } = res;
-                setFectch(data);
-                console.log(fectch);
-                console.log(data);
-                setLoading(false)
-            })
-    }, [select])
-    console.log(fetch)
+    const [selectbusiness, setSelectbusiness] = useState()
+    const [address, setAddress] = useState('')
+    const formik = useFormik({
+        initialValues: {
+            // accountID: user?.id,
+            // numOfRecruit: "",
+            // offerEndTime: Date.now(),
+            // salary: "",
+            // age: "",
+            // startTime: "",
+            // endTime: "",
+            // jobDescription: "",
+            // other: "",
+            // business: "",
+            // location: "",
+            // province: "",
+            address: "",
+            // degree: "",
+            // jobType: "",
+        },
+        validationSchema: Yup.object({
+            numOfRecruit: Yup.number().required().typeError("xin vui lòng nhập số"),
+            offerEndTime: Yup.date().required().min(new Date(), "Ngày kết thúc phải lớn hơn ngày hiện tại"),
+            salary: Yup.number().required().typeError("xin vui lòng nhập lương"),
+            age: Yup.number().required().typeError("xin vui lòng nhập số tuổi"),
+            startTime: Yup.string().required().typeError("xin vui lòng nhập giờ bắt đầu").min(1, "xin vui lòng nhập đúng định dạng giờ").max(24, "xin vui lòng nhập đúng định dạng giờ"),
+            endTime: Yup.string().required().typeError("xin vui lòng nhập giờ kết thúc"),
+            jobDescription: Yup.string().required().typeError("xin vui lòng nhập mô tả"),
+            other: Yup.string(),
+            business: Yup.string().typeError("xin vui lòng chọn công ty"),
+            location: Yup.number(),
+            province: Yup.string(),
+            address: Yup.string().typeError("xin vui lòng nhập địa chỉ"),
+            degree: Yup.string().required(),
+            jobType: Yup.string().required(),
+        }),
+        onSubmit: (values) => {
+            console.log(values)
+            alert(JSON.stringify(values, null, 2));
+            const user = JSON.parse(localStorage.getItem("FWApp-gig:rememberedAccount"));
+            const data = {
+                accountID: user?.id,
+                numOfRecruit: values.numOfRecruit,
+                offerEndTime: values.offerEndTime,
+                salary: values.salary,
+                age: values.age,
+                startTime: values.startTime,
+                endTime: values.endTime,
+                jobDescription: values.jobDescription,
+                other: values.other,
+                business: values.business,
+                location: values.location,
+                province: values.province,
+                address: values.address,
+                degree: values.degree,
+                jobType: values.jobType,
+            }
+            console.log(data)
+            // const url = "https://gig-worker-backend.azurewebsites.net/JobOffer/CreateJO";
+            // axios.post(url, data).then((res) => {
+            //     console.log(res)
+            //     if (res.status === 200) {
+            //         alert("Tạo thành công")
+            //     }
+            // })
+        },
+        onChange: (e) => {
+            setData({ ...data, [e.target.name]: e.target.value })
+            console.log(data)
+        }
+    });
     useEffect(() => {
         setLoading(true)
         const fetchData = async () => {
@@ -37,13 +99,25 @@ function CreateJO() {
         }
         fetchData();
     }, []);
-    console.log(repo)
     const inputsHandler = (e) => {
         setData({ ...data, [e.target.name]: e.target.value })
     }
     const selectLocation = (e) => {
-        setSelect(e.target.value)
+        axios.get(`https://gig-worker-backend.azurewebsites.net/Location/City?province=${e.target.value}`)
+            .then((res) => {
+                const { data } = res;
+                setFectch(data);
+                console.log(fectch);
+                console.log(data);
+                setLoading(false)
+            })
         setData({ ...data, [e.target.name]: e.target.value })
+    }
+    const selectBusiness = (e) => {
+        setSelectbusiness(e.target.value)
+    }
+    const inputaddress = (e) => {
+        setAddress(e.target.value)
     }
     // console.log(select)
     // console.log(fetch)
@@ -78,11 +152,10 @@ function CreateJO() {
             </Box>
             <Box className='form-create-job'
                 component="form"
-                onSubmit={handleSubmit}
+                onSubmit={formik.handleSubmit}
                 sx={{
                     '& > :not(style)': { m: 1, width: '50ch' },
                 }}
-                noValidate
                 autoComplete="off"
             >
                 {/*các field cần có:
@@ -104,7 +177,8 @@ function CreateJO() {
                 <TextField
                     select
                     label="Chọn loại công việc"
-                    onChange={inputsHandler}
+                    value={formik.values.jobType}
+                    onChange={formik.handleChange}
                     name='jobType'>
                     {repo?.jobNames?.map((option) => (
                         <MenuItem key={option?.typeID} value={option?.typeID}>
@@ -112,10 +186,12 @@ function CreateJO() {
                         </MenuItem>
                     ))}
                 </TextField>
+                {formik.errors.jobType ? <div>{formik.errors.jobType}</div> : null}
                 <TextField
                     select
                     label="Chọn bằng cấp"
-                    onChange={inputsHandler}
+                    value={formik.values.degree}
+                    onChange={formik.handleChange}
                     name='degree'>
                     {repo?.degreeNames?.map((option) => (
                         <MenuItem key={option?.degreeID} value={option?.degreeID}>
@@ -123,62 +199,108 @@ function CreateJO() {
                         </MenuItem>
                     ))}
                 </TextField>
-
-
+                {formik.errors.degree ? <div>{formik.errors.degree}</div> : null}
                 <p>*Nếu nơi ứng tuyển không phải là doanh nghiệp hãy điền rõ địa chỉ ở ô bên dưới:</p>
-                <TextField label="Địa chỉ:" variant="standard" onChange={inputsHandler} name='address' />
-                <TextField
-                    select
-                    label="Chọn địa chỉ doanh nghiệp"
-                    onChange={inputsHandler}
-                    name='business'>
-                    {repo?.businessAddresses?.map((option) => (
-                        <MenuItem key={option?.businessID} value={option?.businessID}>
-                            {option?.address}
-                        </MenuItem>
-                    ))}
-                </TextField>
-                <TextField
-                    select
-                    label="Chọn Tỉnh"
-                    // value={select}
-                    onChange={selectLocation}
-                    name='province'>
-                    {provivince.map((option) => (
-                        <MenuItem key={option} value={option} >
-                            {option}
-                        </MenuItem>
-                    ))}
-                </TextField>
-                <TextField
-                    select
-                    label="Chọn Thành phố/Quận/Huyện"
-                    onChange={inputsHandler}
-                    name='location'>
-                    {fectch?.map((option) => (
-                        <MenuItem key={option?.locationID} value={option?.locationID}>
-                            {option?.city}
-                        </MenuItem>
-                    ))}
-                </TextField>
-                {/* <Autocomplete
-                    disablePortal
-                    options={fectch}
-                    sx={{ width: 300 }}
-                    name='location'
-                    value={fetch?.locationID}
-                    renderInput={(params) => <TextField {...params} label="Chọn Thành phố/Quận/Huyện" />}
-                /> */}
+                <TextField label="Địa chỉ:" variant="standard" value={formik.values.address} onChange={formik.handleChange} name='address' />
+                {formik.values.address != '' ?
+                    <>
+                        <TextField
+                            select
+                            label="Chọn Tỉnh"
+                            // value={select}
+                            onChange={selectLocation}
+                            name='province'>
+                            {provivince.map((option) => (
+                                <MenuItem key={option} value={option} >
+                                    {option}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        <TextField
+                            select
+                            label="Chọn Thành phố/Quận/Huyện"
+                            onChange={inputsHandler}
+                            name='location'>
+                            {fectch?.map((option) => (
+                                <MenuItem key={option?.locationID} value={option?.locationID}>
+                                    {option?.city}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                    </> : <TextField
+                        select
+                        label="Chọn địa chỉ doanh nghiệp"
+                        onChange={setSelectbusiness}
+                        name='business'>
+                        {repo?.businessAddresses?.map((option) => (
+                            <MenuItem key={option?.businessID} value={option?.businessID}>
+                                {option?.address}
+                            </MenuItem>
+                        ))}
+                    </TextField>}
+
+
+
+
 
                 <h4>Thông tin chi tiết về công việc:</h4>
-                <TextField required label="Số lượng cần tuyển:" variant="standard" onChange={inputsHandler} name='numOfRecruit' />
-                <TextField required label="Thời hạn kết thúc đăng tuyển:" variant="standard" onChange={inputsHandler} name='offerEndTime' />
-                <TextField label="Lương (theo giờ):" variant="standard" onChange={inputsHandler} name='salary' />
-                <TextField label="Độ tuổi tối thiểu:" variant="standard" onChange={inputsHandler} name='age' />
-                <TextField label="Thời gian bắt đầu làm việc:" variant="standard" onChange={inputsHandler} name='startTime' />
-                <TextField label="Thời gian kết thúc công việc:" variant="standard" onChange={inputsHandler} name='endTime' />
-                <TextField label="Mô tả công việc:" variant="standard" onChange={inputsHandler} name='jobDescription' />
-                <TextField label="Yêu cầu khác:" variant="standard" onChange={inputsHandler} name='other' />
+                <TextField required label="Số lượng cần tuyển:" variant="standard" value={formik.values.numOfRecruit} onChange={formik.handleChange} name='numOfRecruit' />
+                {formik.errors.numOfRecruit ? <div>{formik.errors.numOfRecruit}</div> : null}
+                <TextField
+                    format="dd/mm/yyyy"
+                    label="Ngày kết thúc đăng tuyển"
+                    name='offerEndTime'
+                    type="date"
+                    value={formik.values.offerEndTime}
+                    onChange={formik.handleChange}
+                    sx={{ width: 220 }}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+                {formik.errors.offerEndTime ? <div>{formik.errors.offerEndTime}</div> : null}
+                <TextField label="Lương (theo giờ):" variant="standard" onChange={formik.handleChange} defaultValue={formik.values.salary} name='salary' />
+                {formik.errors.salary ? <div>{formik.errors.salary}</div> : null}
+                <TextField label="Tuổi:" variant="standard" onChange={formik.handleChange} defaultValue={formik.values.age} name='age' />
+                {formik.errors.age ? <div>{formik.errors.age}</div> : null}
+                <TextField
+                    id="time"
+                    name='startTime'
+                    label="Thời gian bắt đầu"
+                    type="time"
+                    defaultValue="07:30"
+                    value={formik.values.startTime}
+                    onChange={formik.handleChange}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    inputProps={{
+                        step: 300, // 5 min
+                    }}
+                    sx={{ width: 150 }}
+                />
+                {formik.errors.startTime ? <div>{formik.errors.startTime}</div> : null}
+                <TextField
+                    id="time"
+                    name='endTime'
+                    label="Thời gian kết thúc"
+                    type="time"
+                    defaultValue="07:30"
+                    value={formik.values.endTime}
+                    onChange={formik.handleChange}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    inputProps={{
+                        step: 300, // 5 min
+                    }}
+                    sx={{ width: 150 }}
+                />
+                {formik.errors.endTime ? <div>{formik.errors.endTime}</div> : null}
+                <TextField label="Mô tả công việc:" variant="standard" onChange={formik.handleChange} defaultValue={formik.values.jobDescription} name='jobDescription' />
+                {formik.errors.jobDescription ? <div>{formik.errors.jobDescription}</div> : null}
+                <TextField label="Yêu cầu khác:" variant="standard" onChange={formik.handleChange} defaultValue={formik.values.other} name='other' />
+                {formik.errors.other ? <div>{formik.errors.other}</div> : null}
 
                 <Button className='submit-button' type='submit'>Đăng tuyển</Button>
             </Box>
